577379c2957bd24da2fe54ddc89d3d5e4f35fc3e
Gabriel Pereyra <Gabriel@PC-Gabo.(none)>
2018-10-01 18:52:57 -0300

    Cambios

--- OblPR2018/OblPR.Client/ClientCommand.cs
+++ OblPR2018/OblPR.Client/ClientCommand.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace OblPR.Client
+﻿namespace OblPR.Client
 {
     public static class ClientCommand
     {
--- OblPR2018/OblPR.Data.Services/ILoginManager.cs
+++ OblPR2018/OblPR.Data.Services/ILoginManager.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using OblPR.Data.Entities;
+﻿using OblPR.Data.Entities;
 
 namespace OblPR.Data.Services
 {
