6d36fe3cee6bca46bcf20218f6abe55a3d25f35c
Gabriel Pereyra <Gabriel@PC-Gabo.(none)>
2018-11-03 18:38:39 -0300

    ABM Player

---
+++ OblPR2018/OblPR.WebService/Models/AddPlayerModel.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations;
+
+namespace OblPR.WebService
+{
+    public class AddPlayerModel
+    {
+        [Required(AllowEmptyStrings = false)]
+        public string Nick;
+        [Required(AllowEmptyStrings = false)]
+        public string Image;
+
+        public AddPlayerModel()
+        {
+
+        }
+    }
+}
